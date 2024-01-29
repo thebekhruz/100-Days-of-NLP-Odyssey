bde0d5422ba1386bc4b424de3d07c82cf3e28298
Bekhruz <bekhruz.suleymanov@student.manchester.ac.uk>
2024-01-29 20:04:55 +0000

    100Days of LeetCode. #Day1

---
+++ Leetcode /Arrays/prob1.py
@@ -0,0 +1,13 @@
+def removeDuplicates(nums) -> int:
+    replace = 1
+    for i in range(1, len(nums)):
+        if nums[i-1] != nums[i]:
+            nums[replace] = nums[i]
+            replace +=1
+
+    return replace, nums
+
+
+mylist =[1,1,2,2,2,3,3,3]
+x = removeDuplicates(mylist)
+print(x)
