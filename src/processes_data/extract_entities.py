0aba64b3632ff0533fdb7bb7bee17eb637da72ce
Bekhruz <thebekhruz@gmail.com>
2024-02-07 22:43:57 +0000

    TODO: Write final functions to group simmilar documents together.

--- src/processes_data/extract_entities.py
+++ src/processes_data/extract_entities.py
@@ -1,20 +1,57 @@
 import pandas as pd
+import numpy as np
 import spacy
+from sklearn.cluster import KMeans
 
 
 file_path = '/Users/thebekhruz/Desktop/100Days-Of-Code/100-Days-of-NLP-Odyssey/data/raw_data/sample_data.csv'
-# Specify your new output file path
 output_file_path = '/Users/thebekhruz/Desktop/100Days-Of-Code/100-Days-of-NLP-Odyssey/data/processed_data/vectorized_data.csv'
 
 
+
+
 df = pd.read_csv(file_path, delimiter='\t', header=None, names=['doc_id', 'type', 'value'])
+nlp = spacy.load("en_core_web_lg")
 
 
-# Load spaCy model with NER
-nlp = spacy.load("en_core_web_sm")
+
+# Preprocessing function
+def preprocess_text(text):
+    text = text.lower()
+    doc = nlp(text)
+    tokens = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct]
+    return " ".join(tokens)
+
+# Use SpaCy to get word embeddings that capture the semantic meaning
+def get_document_embedding(text):
+    # Preprocess the text first
+    preprocessed_text = preprocess_text(text)
+    doc = nlp(preprocessed_text)
+    vectors = [token.vector for token in doc if token.has_vector]
+    if vectors:
+        # Average the vectors to get the document embedding
+        document_embedding = np.mean(vectors, axis=0)
+    else:
+        # If no tokens have vectors, return a zero vector
+        document_embedding = np.zeros((nlp.vocab.vectors_length,))
+    return document_embedding
+
+
+def k_cluster(document_embeddings, n_clusters: int):
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+    labels = kmeans.fit_predict(document_embeddings)
+    return labels, kmeans.cluster_centers_
+
+
+def get_texts():
+    # Directly filter and concatenate 'title' and 'description' rows in one step
+    combined = df[df['type'].isin(['title', 'description'])]['value']
+    return combined
+
+
 
 # Function to preprocess text and extract entities
-def extract_entities(text):
+""" def extract_entities(text):
     doc = nlp(text)
     entities = set()  # Use a set to avoid duplicate entities
     for ent in doc.ents:
@@ -41,5 +78,5 @@ df['category'] = df['entities'].apply(lambda x: max(set(x), key=x.count) if isin
 
 # show first 10 entities
 print(df['category'].head())
-
+ """
 # At this point, df['entities'] contains lists of entity labels extracted from each document
