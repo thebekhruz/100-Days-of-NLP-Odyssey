652a4eca047bfb62ac1b43e42d980f70355f0b85
Bekhruz <thebekhruz@gmail.com>
2024-02-09 16:20:57 +0000

    ✅ Topic Modelling Completed. 🛑 TODO: Hypertune the model. 🛑 Develop a program that will show the optimal number of topics. 🛑 Optimise the program.

--- src/processes_data/topic_modelling.py
+++ src/processes_data/topic_modelling.py
@@ -3,14 +3,31 @@ import gensim
 import gensim.corpora as corpora
 from gensim.utils import simple_preprocess
 from gensim.models import CoherenceModel
+from gensim.models.ldamodel import LdaModel
+from gensim.corpora import Dictionary
+
+import re
+import numpy as np
 import pandas as pd
+from pprint import pprint
 
-# spacy for lemmatization
+# spacy
 import spacy
-nlp = spacy.load("en_core_web_lg")
+nlp = spacy.load("en_core_web_md", disable=["ner", "parser"])
 
-def preprocess_text(text):
-    return " ".join(token.lemma_ for token in nlp(text.lower()) if not token.is_stop and not token.is_punct)
+# Visualisation
+import pyLDAvis
+import pyLDAvis.gensim_models as gensimvis
+import time
+
+def preprocess_texts(texts):
+    """ Batch process a list of texts, applying preprocessing steps such as lemmatization and stopword removal."""
+    processed_texts = []
+    texts = [str(text) for text in texts]
+    for doc in nlp.pipe(texts, batch_size=20):
+        processed_texts.append([token.lemma_ for token in doc if not token.is_stop and not token.is_punct])
+    return processed_texts
+
 
 # Build the bigram and trigram models
 def build_bigram_trigram_models(texts):
@@ -19,20 +36,16 @@ def build_bigram_trigram_models(texts):
     return gensim.models.phrases.Phraser(bigram), gensim.models.phrases.Phraser(trigram)
 
 
-def make_bigrams(texts, bigram_mod):
-    return [bigram_mod[doc] for doc in texts]
-
-
-def make_trigrams(texts, bigram_mod, trigram_mod):
+def apply_bigram_trigram(texts, bigram_mod, trigram_mod):
     return [trigram_mod[bigram_mod[doc]] for doc in texts]
 
 
-def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
-    texts_out = []
-    for sent in texts:
-        doc = nlp(" ".join(sent)) 
-        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
-    return texts_out
+def make_bigrams(texts, bigram_mod):
+    return [bigram_mod[doc.split()] for doc in texts]
+
+
+def make_trigrams(texts, bigram_mod, trigram_mod):
+    return [trigram_mod[bigram_mod[doc.split()]] for doc in texts]
 
 
 def get_texts_and_ids(df):
@@ -41,25 +54,58 @@ def get_texts_and_ids(df):
     return filtered
 
 
+def get_texts(df):
+    return df[df['type'].isin(['title', 'description'])]['value'].tolist()
+
+def create_dictionary_and_corpus(texts):
+    id2word = corpora.Dictionary(texts)
+    id2word.filter_extremes(no_below=5, no_above=0.5)                      # Remove extremely rare and common tokens
+    corpus = [id2word.doc2bow(text, allow_update=True) for text in texts]  # Efficiently update dictionary
+    return id2word, corpus
+
+
+def build_lda_model(corpus, dictionary, num_topics=30):
+    return LdaModel(corpus=corpus,
+                         id2word=dictionary,
+                         num_topics=num_topics,
+                         random_state=100,
+                         update_every=1,
+                         chunksize=1000,
+                         passes=10,
+                         alpha='auto',
+                         per_word_topics=True)
+
+
+
 def main():
+    start_time = time.time()  # Start time measurement
 
-    file_path = 'data/raw_data/sample_data.csv'
+    # file_path = 'data/raw_data/chunk/partition_2500ent.csv'
+    file_path = 'data/raw_data/swop_triples.csv'
     df = pd.read_csv(file_path, delimiter='\t', header=None, names=['doc_id', 'type', 'value'])
-
+    texts = get_texts(df)
     
-    texts =get_texts_and_ids(df)
-    processed_text = texts['value'].apply(preprocess_text)
-    bigram_mod, trigram_mod = build_bigram_trigram_models(processed_text)
 
-    # Form Bigrams
-    data_words_bigrams = make_bigrams(processed_text, bigram_mod)
-    data_lemmatized = lemmatization(data_words_bigrams, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
+    processed_texts = preprocess_texts(texts)
+    bigram_mod, trigram_mod = build_bigram_trigram_models(processed_texts)
+    data_with_bigrams_trigrams = apply_bigram_trigram(processed_texts, bigram_mod, trigram_mod)
 
-
-    print(data_lemmatized[:1])
+ 
+    id2word, corpus = create_dictionary_and_corpus(data_with_bigrams_trigrams)
+    lda_model = build_lda_model(corpus = corpus, dictionary =id2word, num_topics = 6)
 
 
-    # return bigram_mod, trigram_mod
 
-biagram, _ = main()
-# print(biagram[:1])
+    # pprint(lda_model.print_topics(-1))
+    coherence_model_lda = CoherenceModel(model=lda_model, texts=data_with_bigrams_trigrams, dictionary=id2word, coherence='c_v')
+    print('\nCoherence Score: ', coherence_model_lda.get_coherence())
+
+    vis = gensimvis.prepare(lda_model, corpus, id2word)
+    pyLDAvis.save_html(vis, 'lda_visualization1.html')
+
+    end_time = time.time()  # End time measurement
+    print(f"Execution Time: {end_time - start_time} seconds")  # Print execution time 
+
+
+if __name__ == "__main__":
+    main()
