ce7c488a43afaa8c58361385fcc085884dcfddd9
Bekhruz <thebekhruz@gmail.com>
2024-02-25 22:34:41 +0000

    🛑 Develop a better form

--- my_app/my_app.py
+++ my_app/my_app.py
@@ -11,26 +11,9 @@ def load_search_page(db_connection):
     ui = StreamlitUI(db_connection)
 
     # Initialize CypherQueryBuilder instance
-    query_builder = CypherQueryBuilder()
     label, date_from, date_to, num_results, variable_name, variable_value = ui.display_query_form()
+    ui.construct_and_display_query(label, variable_name, variable_value, date_from, date_to, num_results)
 
-    constructed_query = ""  # Initialize to avoid reference before assignment
-
-    # Construct and display the query based on user inputs
-    if st.button('Construct and Display Query'):
-        # Check if specific variable search is enabled
-        if variable_name and variable_value:
-            constructed_query = query_builder.construct_variable_query(label, variable_name, variable_value, date_from, date_to, num_results)
-        elif label and (date_from or date_to):
-            # Assuming there's a method to handle this case
-            constructed_query = query_builder.construct_query(label, None, date_from, date_to, num_results)
-        else:
-            # Handle other cases or show a message indicating that more input is needed
-            st.write("Please provide more input to construct the query.")
-
-        if constructed_query:  # Ensure we have a query to display
-            st.code(constructed_query)  # Display the constructed query
-            st.session_state['constructed_query'] = constructed_query  # Save in session state
 
     # Execute custom or constructed query
     custom_query = ui.display_direct_query_input()
@@ -38,8 +21,6 @@ def load_search_page(db_connection):
     with execute_section:
         if st.button('Execute Custom Query'):
             ui.execute_query(custom_query)
-        elif 'constructed_query' in st.session_state and st.button('Execute Constructed Query'):
-            ui.execute_query(st.session_state['constructed_query'])
 
 
 
